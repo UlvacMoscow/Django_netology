5396e0ffa1cdb0fd8802b3b3245f02abe311d87a
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-21 22:19:04 +0300

    add file content

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -5,7 +5,7 @@ from app.views import FileList, file_content
 
 urlpatterns = [
     # # Определите схему урлов с привязкой к отображениям .views.FileList и .views.file_content
-    path('', FileList.as_view()),
-    path('/<int:year>-<int:month>-<int:day>/', FileList.as_view()),   
-    path('file/<str:name>/', file_content ),
+    path('', FileList.as_view(), name='file_list'),
+    path('<int:year>-<int:month>-<int:day>/', FileList.as_view()),
+    path('file/<str:name>/', file_content, name='file_content' ),
 ]
--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,5 +1,6 @@
 import datetime
-
+from .settings import FILES_PATH
+import os
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -23,12 +24,20 @@ class FileList(TemplateView):
 # http://127.0.0.1:5000/name
 
 def file_content(request, name):
+    files = os.listdir(FILES_PATH)
     print(request)
-    print(name) #rrrrr
-
+    print(name)
+    print(files) #rrrrr
+    print(FILES_PATH)
+    for file in files:
+        if file == name:
+            with open(os.path.join(FILES_PATH, file), encoding='utf8') as necessary_file:
+                file_content = necessary_file.read()
+        else:
+            file_content = 'такого файла нет'
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
     return render(
         request,
         'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
+        context={'file_name': name, 'file_content': file_content}
     )
