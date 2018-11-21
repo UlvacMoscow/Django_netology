c29e433e90ac73ca554c0f11ce18b7484389ac53
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-21 23:04:12 +0300

    update file content

--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -18,26 +18,20 @@ class FileList(TemplateView):
             ],
             'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
         }
-    def postllk():
-        pass
 
-# http://127.0.0.1:5000/name
+
 
 def file_content(request, name):
     files = os.listdir(FILES_PATH)
-    print(request)
-    print(name)
-    print(files) #rrrrr
-    print(FILES_PATH)
+    file_content = 'такого файла нету'
+    
     for file in files:
         if file == name:
             with open(os.path.join(FILES_PATH, file), encoding='utf8') as necessary_file:
                 file_content = necessary_file.read()
-        else:
-            file_content = 'такого файла нет'
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
+
     return render(
-        request,
-        'file_content.html',
-        context={'file_name': name, 'file_content': file_content}
-    )
+               request,
+               'file_content.html',
+               context={'file_name': name, 'file_content':file_content}
+           )
