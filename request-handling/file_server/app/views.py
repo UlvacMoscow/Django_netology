22e67791fb9309002a672f0d56cdc5aaca1879ab
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-22 16:26:13 +0300

    update file content

--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -7,16 +7,29 @@ from django.views.generic import TemplateView
 
 class FileList(TemplateView):
     template_name = 'index.html'
+    print('111111111111111111111')
+
 
     def get_context_data(self, date=None):
-        # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
+        file_list = []
+        files = os.listdir(FILES_PATH)
+        print(files)
+        for file in files:
+            file_way = os.path.join(FILES_PATH, file)
+            file_info = {
+                'name': file,
+                'ctime': datetime.datetime.fromtimestamp(os.stat(file_way).st_ctime).strftime("%Y %m, %d"),
+                'mtime': os.stat(file_way).st_mtime}
+            file_list.append(file_info)
+            print('--------------+++++++++++++++++++++')
+        print(file_list)
+        # datetime.datetime.fromtimestamp(os.stat(file_way).st_ctime).strftime("%Y %m, %d")
+        # if not date:
+        #     print(files)
+    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
         return {
-            'files': [
-                {'name': 'file_name_1.txt',
-                 'ctime': datetime.datetime(2018, 1, 1),
-                 'mtime': datetime.datetime(2018, 1, 2)}
-            ],
-            'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
+            'files': file_list
+            # 'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
         }
 
 
@@ -24,7 +37,7 @@ class FileList(TemplateView):
 def file_content(request, name):
     files = os.listdir(FILES_PATH)
     file_content = 'такого файла нету'
-    
+
     for file in files:
         if file == name:
             with open(os.path.join(FILES_PATH, file), encoding='utf8') as necessary_file:
