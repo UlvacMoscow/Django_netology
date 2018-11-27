00e7edb848ab610b8126bf1a3f06e764b3cb42d2
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-28 00:19:30 +0300

    task1/views complete v3

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -15,19 +15,44 @@ class InflationView(TemplateView):
         all_stats = []
 
         with open (INFLATION_RUSSIA, encoding='utf8') as csvfile:
+            # dictreader = csv.DictReader(csvfile)
+            # print('dictreader ', dictreader)
+            # for row in dictreader:
+            #     # print(row['Год'])
+            #     print(list(row)[0])
+            #     print('1 ', row[0])
+
+
+
+
+
+
+
+
             reader = csv.reader(csvfile, delimiter = ';')
             reader = list(reader)
             temp = reader.pop(0)
-            for stat_row in reader:
+            convert_float = lambda x: '-' if '' else float(x)
+            for stat_row in reader:  # reader[1:] со второго элемента
+                print('stat_row ',stat_row)
                 full_row = {}
                 for num in range(len(temp)):
-                    full_row[temp[num]] = stat_row[num]
+                    if num == 0:
+                        full_row[temp[num]] = stat_row[num]
+                    elif stat_row[num] == '':
+                        full_row[temp[num]] = '-'
+                    elif float(stat_row[num]) % 1 == 0:
+                        full_row[temp[num]] = int(stat_row[num])
+                    else:
+                        full_row[temp[num]] = float(stat_row[num])
                 all_stats.append(full_row)
 
         for st in all_stats:
             print(st)
 
         # чтение csv-файла и заполнение контекста
-        context = {}
+        context = {'inflation' : all_stats,
+                   'columns' : temp,
+                   'zero' : 0.31 }
         return render(request, self.template_name,
                       context)
