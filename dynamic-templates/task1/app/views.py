ed4166ec4b6b07eac2ec0dfcdb69927c4fd2a948
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-11-27 20:43:21 +0300

    task1/views complete v2

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -2,6 +2,7 @@ import csv
 from .settings import INFLATION_RUSSIA
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import collections
 
 
 
@@ -9,10 +10,23 @@ from django.views.generic import TemplateView
 class InflationView(TemplateView):
     template_name = 'inflation.html'
 
+
     def get(self, request, *args, **kwargs):
-        with open (INFLATION_RUSSIA, encoding=None) as csvfile:
-            reader = csv.DictReader(csvfile)
-            
+        all_stats = []
+
+        with open (INFLATION_RUSSIA, encoding='utf8') as csvfile:
+            reader = csv.reader(csvfile, delimiter = ';')
+            reader = list(reader)
+            temp = reader.pop(0)
+            for stat_row in reader:
+                full_row = {}
+                for num in range(len(temp)):
+                    full_row[temp[num]] = stat_row[num]
+                all_stats.append(full_row)
+
+        for st in all_stats:
+            print(st)
+
         # чтение csv-файла и заполнение контекста
         context = {}
         return render(request, self.template_name,
