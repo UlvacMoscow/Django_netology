14aa9a910031e88a776b407ec19e636ebabacfd1
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-12-04 18:23:38 +0300

    correct to error to file_server

--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Phone(models.Model):
+    brand = models.CharField('brand', max_length=40)
+    model_name = models.CharField('model_name', max_length=50)
+    screen = models.CharField('screen', max_lenght=5)
+    camera = models.BooleanField('camera', default=True)
+    battery
+    memory
--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -3,6 +3,7 @@ from .settings import FILES_PATH
 import os
 from django.shortcuts import render, render_to_response
 from django.views.generic import TemplateView
+from dateutil.parser import parse
 
 
 
@@ -12,6 +13,10 @@ class FileList(TemplateView):
     def get_context_data(self, date=None, **kwargs):
         context = super().get_context_data(**kwargs)
         file_list = []
+        if date != None:
+            get_date_obj = parse(date).strftime("%Y-%m-%d")
+
+
         for file in os.listdir(FILES_PATH):
             file_stats = os.stat(os.path.join(FILES_PATH, file))
             file_info = {
@@ -20,9 +25,8 @@ class FileList(TemplateView):
                 'mtime': dt.fromtimestamp(file_stats.st_mtime),
             }
 
-
-            if date == None or date[:10] == dt.fromtimestamp(file_stats.st_ctime).strftime("%Y-%m-%d") \
-             or date[:10] == dt.fromtimestamp(file_stats.st_mtime).strftime("%Y-%m-%d"):
+            if date == None or get_date_obj == dt.fromtimestamp(file_stats.st_ctime).strftime("%Y-%m-%d") \
+                or get_date_obj == dt.fromtimestamp(file_stats.st_mtime).strftime("%Y-%m-%d"):
                 file_list.append(file_info)
 
         context.update({
@@ -34,15 +38,24 @@ class FileList(TemplateView):
 
 def file_content(request, name):
     files = os.listdir(FILES_PATH)
-    file_content = 'такого файла нету'
-
     for file in files:
         if file == name:
-            with open(os.path.join(FILES_PATH, file), encoding='utf8') as necessary_file:
-                file_content = necessary_file.read()
+                with open(os.path.join(FILES_PATH, name), encoding='utf8') as necessary_file:
+                    file_content = necessary_file.read()
 
-    return render(
-               request,
-               'file_content.html',
-               context={'file_name': name, 'file_content':file_content}
-           )
+
+    try:
+        return render(
+                   request,
+                   'file_content.html',
+                   context={'file_name': name, 'file_content': file_content}
+               )
+    except UnboundLocalError:
+        name = 'Файл {} не существует'.format(name)
+        file_content = ''
+        context={'file_name': name, 'file_content': file_content}
+        return render(
+                   request,
+                   'file_content.html',
+                   context={'file_name': name, 'file_content': file_content}
+               )
