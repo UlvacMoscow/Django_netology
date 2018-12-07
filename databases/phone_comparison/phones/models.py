584b69990f0fc0920f7345e06e954a40c230f44a
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-12-07 20:10:16 +0300

    create models: phone, nokia, xiomi

--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -2,9 +2,29 @@ from django.db import models
 
 # Create your models here.
 class Phone(models.Model):
-    brand = models.CharField('brand', max_length=40)
-    model_name = models.CharField('model_name', max_length=50)
-    screen = models.CharField('screen', max_lenght=5)
+    model_name = models.CharField('model_name', max_length=40)
+    screen = models.CharField('screen', max_length=5)
     camera = models.BooleanField('camera', default=True)
-    battery
-    memory
+    battery = models.IntegerField('battery', default=100)
+    memory = models.IntegerField('memory', default=0)
+
+    def __str__(self):
+        return self.model_name
+
+
+class Nokia(models.Model):
+    parametrs = models.ForeignKey(Phone, on_delete=models.CASCADE)
+    name = models.CharField('name', max_length=50, default='Nokia')
+    scan_eye = models.BooleanField('scan_eye', default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Xiomi(models.Model):
+    parametrs = models.ForeignKey(Phone, on_delete=models.CASCADE)
+    name = models.CharField('name', max_length=50, default='Xiomi')
+    scan_finger = models.BooleanField('scan_finger', default=False)
+
+    def __str__(self):
+        return self.name
