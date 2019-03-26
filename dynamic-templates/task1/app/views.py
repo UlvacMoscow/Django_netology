c0ca12db40fca7763b3f663ac0c18c1e3f426bd8
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-03-26 22:41:26 +0300

    dictReader

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -14,9 +14,16 @@ class InflationView(TemplateView):
     def get(self, request, *args, **kwargs):
         with open (settings.INFLATION_RUSSIA, encoding='utf8') as csvfile:
             reader = csv.DictReader(csvfile, delimiter = ';')
-            print('111111 ', reader)
-            for row in reader:
-                print(row)
+            print('111111 ', dir(reader))
+            print('22222  ',reader.fieldnames)
+            print('22222  ',reader.__iter__)
+            # for r in reader:
+            #     print(r)
+                # print(r.items())
+                # for it in r.items():
+                #     print(it[1])
+
+
             context = {'reader': reader}
         return render(request, self.template_name,
                       context)
