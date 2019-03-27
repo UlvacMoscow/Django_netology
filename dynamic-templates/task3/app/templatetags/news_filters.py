fd34b303cb255a45b5c73e296cbb21796c0f156b
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-03-27 22:48:47 +0300

    update task3

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,24 +1,25 @@
 from django import template
 from datetime import datetime as dt
 
-
 register = template.Library()
-
 
 @register.filter
 def format_date(value):
     now = dt.now()
+    print('111111111 ', now)
     then = dt.fromtimestamp(value)
+    print('1111111112 ', then)
     delta = now - then
+    print('1111111113 ', delta)
     hours = delta.seconds // 3600
+    print('1111111114 ', dir(hours))
 
     if delta.seconds // 60 < 10:
         return 'Только что'
     elif hours < 24:
         return '{} часов назад'.format(hours)
-    else:
-        return dt.fromtimestamp(delta).strftime("%Y-%m-%d")
-    return then
+    return dt.fromtimestamp(delta).strftime("%Y-%m-%d")
+
 
 
 @register.filter
@@ -27,8 +28,7 @@ def format_score(value=0):
         return 'все плохо'
     elif value > 5:
         return 'хорошо'
-    else:
-        return 'нейтрально'
+    return 'нейтрально'
 
 
 @register.filter
@@ -37,14 +37,12 @@ def format_num_comments(value):
         return 'Оставьте комментарий'
     elif 0 < value < 50:
         return value
-    else:
-        return '50+'
-    return value
+    return '50+'
 
 
 @register.filter
 def format_selftext(text, count):
     temp = text.split()
-    if len(temp) - 1 > 2 * count:
+    if len(temp) > 2 * count:
         return '{0} ... {1}'.format(' '.join(temp[:count]), ' '.join(temp[-count:]))
     return text
