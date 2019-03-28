19354d53858be6c10e7dcd2a75b0f35bb3552e66
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-03-28 22:00:35 +0300

    update phone models

--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Phone(models.Model):
     model_name = models.CharField('model_name', max_length=40)
+    brand = models.CharField('brand', max_length=40)
     screen = models.CharField('screen', max_length=5)
+    processor = models.CharField('processor', max_length=40)
     camera = models.BooleanField('camera', default=True)
     battery = models.IntegerField('battery', default=100)
     memory = models.IntegerField('memory', default=0)
@@ -12,19 +14,18 @@ class Phone(models.Model):
         return self.model_name
 
 
-class Nokia(models.Model):
-    parametrs = models.ForeignKey(Phone, on_delete=models.CASCADE)
-    name = models.CharField('name', max_length=50, default='Nokia')
+class Nokia(Phone):
     scan_eye = models.BooleanField('scan_eye', default=False)
-
     def __str__(self):
         return self.name
 
 
-class Xiomi(models.Model):
-    parametrs = models.ForeignKey(Phone, on_delete=models.CASCADE)
-    name = models.CharField('name', max_length=50, default='Xiomi')
+class Xiaomi(Phone):
     scan_finger = models.BooleanField('scan_finger', default=False)
+    def __str__(self):
+        return self.name
 
+class Asus(Phone):
+    teleport = models.BooleanField('teleport', default=False)
     def __str__(self):
         return self.name
