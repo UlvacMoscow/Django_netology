057c07656b52bdb1dbc141260105fdc1a82fd215
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-04-02 23:09:36 +0300

    add list_book

--- databases/models_list_displaying/books/views.py
+++ databases/models_list_displaying/books/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
+from .models import Book
 
 
 class BookListView(generic.ListView):
+    model = Book
+    template_name = 'book_list.html'
     def get_context_data(self, **kwargs):
-        pass
+        list_book = Book.objects.all()
+        return {'list_book': list_book}
