bd3132451979d48b3aac6f9c22c8c14b75990cd4
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-04-04 23:12:59 +0300

    blow my brain

--- databases/models_list_displaying/books/views.py
+++ databases/models_list_displaying/books/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import get_object_or_404
 class BookListView(generic.ListView):
     model = Book
     template_name = 'book_list.html'
+    # context_object_name = 'book_list'
+    paginate_by = 1
     # def get_context_data(self,request, date=None, **kwargs):
     #     template_name = 'book_date.html'
     #     context = super().get_context_data(**kwargs)
@@ -19,6 +21,7 @@ class BookListView(generic.ListView):
     #     return {'list_book': list_book}
     def get_queryset(self):
         # template_name = 'book_date.html'
-        print(self.request)
-        # self.Book = get_object_or_404(Book, date = self.kwargs['Book'])
-        # return Book.objects.filter(Book=self.Book)
+        # queryset = Book.objects.filter(pub_date='2018-09-05')
+        # print(self.request)
+        self.Book = get_object_or_404(Book, pub_date=self.kwargs['pub_date'])
+        return Book.objects.filter(pub_date=self.Book)
