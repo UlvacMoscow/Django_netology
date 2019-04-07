0441b757044fb02d84dcddd4dda8a53a7effcd17
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-04-07 20:49:35 +0300

    add inline for models

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from .models import ArticleTopic
 
 from .models import Article
 
 
+class ArticleTopicInline(admin.TabularInline):
+    model = Article.article_topic.through
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        ArticleTopicInline,
+    ]
+
+@admin.register(ArticleTopic)
+class ArticleTopicAdmin(admin.ModelAdmin):
+    inlines = [
+        ArticleTopicInline,
+    ]
