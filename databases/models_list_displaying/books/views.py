a5eb056c845c0b730db83ca92bcb475f0f6b14d7
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-04-11 22:22:51 +0300

    all querysets complete

--- databases/models_list_displaying/books/views.py
+++ databases/models_list_displaying/books/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 
 class BookListView(generic.ListView):
     def __init__(self):
+        super().__init__()
         self.model = Book
         self.template_name = 'book_list.html'
 
@@ -15,16 +16,14 @@ class BookListView(generic.ListView):
 
 
 
-class BookFilterDate(generic.ListView):
-    # def __init__(self):
-    #     super().__init__()
-    model = Book
-    template_name = 'book_list.html'
-    # def get_queryset(self):
-    #     super().get_queryset()
+class BookFilterDate(BookListView):
+    def __init__(self):
+        super().__init__()
+
 
     def get_context_data(self):
         category_url = self.kwargs['date']
         queryset = Book.objects.filter(pub_date=category_url)
+        print('111111111111111 ', Paginator)
         context = {'object_list' : queryset}
         return context
