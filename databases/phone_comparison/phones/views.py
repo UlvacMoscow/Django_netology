ed89cf454c632ca6aca88ea5a73ab3ba70c5996f
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-05-19 23:16:06 +0300

    need to lerne how give json or dict from views-ajax to template

--- databases/phone_comparison/phones/views.py
+++ databases/phone_comparison/phones/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from .models import Phone, Asus, Nokia, Xiaomi
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 
 
 def show_catalog(request):
@@ -17,7 +18,6 @@ def show_catalog(request):
 def compression_phones(request, *args, **kwargs):
     phones = Phone.get_all_phones()
     context = {'phones': phones}
-    print('calllll')
     if request.GET.get('phone1'):
         num1 = request.GET.get('phone1')
         num2 = request.GET.get('phone2')
@@ -34,5 +34,18 @@ def compression_phones(request, *args, **kwargs):
 
 
 def ajax(request):
+
+    print(request.GET.get('data'))
+    if request.GET.get('phone_1') and request.GET.get('phone_2'):
+        num1 = request.GET.get('phone_1')
+        num2 = request.GET.get('phone_2')
+        print('1111 ', num1, num2)
+        info_phone1 = Phone.objects.get(pk=num1)
+        info_phone2 = Phone.objects.get(pk=num2)
+        print('2222 ', info_phone1.brand)
+        # return HttpResponse({'brand' : info_phone1.brand})
+        # return JsonResponse({'brand' : info_phone1.brand})
+        return render({'brand' : info_phone1.brand})
     print('ajax 11111111111111111111')
+    # print(request.GET.get('phone_1'))
     return HttpResponse('<h6>Hello</h6>')
