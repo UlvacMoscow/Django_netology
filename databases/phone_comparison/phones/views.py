b87f494fb218b2f8a02360f35355cf2115e0bd08
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-05-21 23:29:00 +0300

    done full task

--- databases/phone_comparison/phones/views.py
+++ databases/phone_comparison/phones/views.py
@@ -34,23 +34,18 @@ def compression_phones(request, *args, **kwargs):
 
 
 def ajax(request):
-
-    print(request.GET.get('data'))
     if request.GET.get('phone_1') and request.GET.get('phone_2'):
         num1 = request.GET.get('phone_1')
         num2 = request.GET.get('phone_2')
-        print('1111 ', num1, num2)
         info_phone1 = Phone.objects.get(pk=num1)
         info_phone2 = Phone.objects.get(pk=num2)
-        print('2222 ', info_phone1.brand)
-        test = {"model_name1" : str(info_phone1.model_name),
-                "brand" : str(info_phone1.brand)
+        test = {"model_name" : info_phone1.model_name,
+                "brand" : info_phone1.brand,
+                "processor" : info_phone1.processor,
+                "model_name2" : info_phone2.model_name,
+                "brand2" : info_phone2.brand,
+                "processor2" : info_phone2.processor
                 }
         test = json.dumps(test)
-        print('json ', test)
-        # return HttpResponse({'brand' : info_phone1.brand})
-        # return JsonResponse({'brand' : info_phone1.brand})
         return HttpResponse(test, content_type='application/json')
-    print('ajax 11111111111111111111')
-    # print(request.GET.get('phone_1'))
-    return HttpResponse('<h6>Hello</h6>')
+    return HttpResponse('<h6>Что-то пошло не так</h6>')
