b518ac37904757ee5d5ff578867f3b1afcb29e0c
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-05-26 17:49:42 +0300

    add paginator, not work

--- databases/models_list_displaying/books/views.py
+++ databases/models_list_displaying/books/views.py
@@ -8,26 +8,33 @@ from django.core.paginator import Paginator
 class BookListView(generic.ListView):
     model = Book
     template_name = 'book_list.html'
+    # paginate_by = 1
 
     def get_queryset(self):
-        print('queryset calllll')
+        if self.kwargs:
+            queryset = Book.objects.filter(pub_date=self.kwargs['date'])
+            return queryset
         queryset = Book.objects.all().order_by('-pub_date')
         return queryset
 
-
+    #
     def get_context_data(self, **kwargs):
-        print('get_context_date calllll')
         context = super().get_context_data(**kwargs)
-        if self.kwargs['date']:
-            print('date url uuuuusssssssssseeeeeeeeee')
-            queryset = Book.objects.filter(pub_date=self.kwargs['date'])
-            context['object_list'] = queryset
-            return context
-        # queryset = Book.objects.filter(pub_date=category_url)
-        query_for_paginate = Book.objects.all().order_by('-pub_date')
-        paginator_class = Paginator(query_for_paginate, 1)
-        # page = paginator_class.get_page(category_url)
-        context = {'paginator' : paginator_class}
-        context['object_list'] = self.get_queryset()
-        # context['books'] = page
-        return context
+        if self.kwargs:
+            books = Book.objects.order_by('pub_date')
+
+            current_book = get_object_or_404(Book, pub_date=self.kwargs['date'])
+            current_page = list(books).index(current_book)
+            count_books = books.count()
+
+            if 0 < current_page < count_books - 1:
+                context['next_date'] = str(list(books)[current_page + 1].pub_date)
+                context['prev_date'] = str(list(books)[current_page - 1].pub_date)
+
+            elif 0 == current_page:
+                context['next_date'] = str(list(books)[current_page + 1].pub_date)
+
+            elif count_books - 1 == current_page:
+                context['prev_date'] = str(list(books)[current_page - 1].pub_date)
+
+                return context
